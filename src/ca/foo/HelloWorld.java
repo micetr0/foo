88ecef33f8945c8f4b401196b05b34871d68aae2
micetr0 <williamhu3@gmail.com>
2014-05-14 19:08:57 -0700

    My first hello_world!!!

---
+++ src/ca/foo/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package ca.foo;
+
+/**
+ * @author a00729057
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+
+
+}
